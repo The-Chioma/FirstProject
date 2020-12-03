c5b6be7d6f308c2d4b97c5aba7ba1a6e1abaab91
Ross McIntyre <rossmci90@hotmail.co.uk>
2020-12-03 20:52:18 +0000

    Added the logic for the random country selector and log the data to the console

--- Assets/scripts/data.js
+++ Assets/scripts/data.js
@@ -1,133 +1,133 @@
 const europe = [
-    {
-        country: "Italy",
-        dish: "Baked Lasagne",
-        language: "Italian",
-        flagCode: "IT",
-        id: 633661
-    },
+  {
+    country: "Italy",
+    dish: "Baked Lasagne",
+    language: "Italian",
+    flagCode: "IT",
+    id: 633661,
+  },
 
-    {
-        country: "France",
-        dish: "Julia Child's Coq au Vin",
-        language: "French",
-        flagCode: "FR",
-        id: 648638
-    },
+  {
+    country: "France",
+    dish: "Julia Child's Coq au Vin",
+    language: "French",
+    flagCode: "FR",
+    id: 648638,
+  },
 
-    {
-        country: "Spain",
-        dish: "Paella Catalane With Mussels, Squid & Crevettes",
-        language: "Spanish",
-        flagCode: "ES",
-        id: 654331
-    },
+  {
+    country: "Spain",
+    dish: "Paella Catalane With Mussels, Squid & Crevettes",
+    language: "Spanish",
+    flagCode: "ES",
+    id: 654331,
+  },
 
-    {
-        country: "Georgia",
-        dish: "Khachapuri",
-        language: "Georgian Kartuli ena",
-        flagCode: "GE",
-        id: 648852
-    },
+  {
+    country: "Georgia",
+    dish: "Khachapuri",
+    language: "Georgian Kartuli ena",
+    flagCode: "GE",
+    id: 648852,
+  },
 
-    {
-        country: "Germany",
-        dish: "Knockwurst with sauerkraut",
-        language: "German",
-        flagCode: "DE",
-        id: 648986
-    },
+  {
+    country: "Germany",
+    dish: "Knockwurst with sauerkraut",
+    language: "German",
+    flagCode: "DE",
+    id: 648986,
+  },
 
-    {
-        country: "Belgium",
-        dish: "Mussels With Tomatoes and Fennel",
-        language: "Dutch",
-        flagCode: "BE",
-        id: 652775,
-    },
+  {
+    country: "Belgium",
+    dish: "Mussels With Tomatoes and Fennel",
+    language: "Dutch",
+    flagCode: "BE",
+    id: 652775,
+  },
 
-    {
-        country: "Switzerland",
-        dish: "Chicken Cordon Bleu",
-        language: "German",
-        flagCode: "CH",
-        id: 638035
-    },
+  {
+    country: "Switzerland",
+    dish: "Chicken Cordon Bleu",
+    language: "German",
+    flagCode: "CH",
+    id: 638035,
+  },
 
-    {
-        country: "Sweden",
-        dish: "Jen's Swedish Meatballs",
-        language: "Swedish",
-        flagCode: "SE",
-        id: 648565
-    },
+  {
+    country: "Sweden",
+    dish: "Jen's Swedish Meatballs",
+    language: "Swedish",
+    flagCode: "SE",
+    id: 648565,
+  },
 ];
 
 const africa = [
-    {
-        country: "Nigeria",
-        dish: "How to Make Party Jollof Rice",
-        language: "English",
-        flagCode: "NG",
-        id: 716298,
-    },
+  {
+    country: "Nigeria",
+    dish: "How to Make Party Jollof Rice",
+    language: "English",
+    flagCode: "NG",
+    id: 716298,
+  },
 
-    {
-        country: "Kenya",
-        dish: "Kenyan Pilau",
-        language: "Swahili",
-        flagCode: "KE",
-        id: 716344
-    },
+  {
+    country: "Kenya",
+    dish: "Kenyan Pilau",
+    language: "Swahili",
+    flagCode: "KE",
+    id: 716344,
+  },
 
-    {
-        country: "Morocco",
-        dish: "Moroccan Chicken Tagine",
-        language: "Arabic",
-        flagCode: "MA",
-        id: 652421
-    },
+  {
+    country: "Morocco",
+    dish: "Moroccan Chicken Tagine",
+    language: "Arabic",
+    flagCode: "MA",
+    id: 652421,
+  },
 
-    {
-        country: "Egypt",
-        dish: "Baba Gonoush (Egyptian Dish)",
-        language: "Arabic",
-        flagCode: "EG",
-        id: 633212
-    },
+  {
+    country: "Egypt",
+    dish: "Baba Gonoush (Egyptian Dish)",
+    language: "Arabic",
+    flagCode: "EG",
+    id: 633212,
+  },
 
-    {
-        country: "Tunisia",
-        dish: "Couscous with olives",
-        language: "Arabic",
-        flagCode: "TN",
-        id: 640238
-    },
+  {
+    country: "Tunisia",
+    dish: "Couscous with olives",
+    language: "Arabic",
+    flagCode: "TN",
+    id: 640238,
+  },
 
-    {
-        country: "Mauritius",
-        dish: "Turkey and Sausage Boulettes",
-        language: "English",
-        flagCode: "MU",
-        id: 663987
-    },
+  {
+    country: "Mauritius",
+    dish: "Turkey and Sausage Boulettes",
+    language: "English",
+    flagCode: "MU",
+    id: 663987,
+  },
 
-    {
-        country: "Mozambique",
-        dish: "Chicken Piri Piri with Spicy Rice",
-        language: "Portuguese",
-        flagCode: "MZ",
-        id: 638252
-    },
+  {
+    country: "Mozambique",
+    dish: "Chicken Piri Piri with Spicy Rice",
+    language: "Portuguese",
+    flagCode: "MZ",
+    id: 638252,
+  },
 
-    {
-        country: "South Africa",
-        dish: "Malay-Style Red Chicken Curry",
-        language: "Zulu",
-        flagCode: "ZA",
-        id: 650673
-    },
+  {
+    country: "South Africa",
+    dish: "Malay-Style Red Chicken Curry",
+    language: "Zulu",
+    flagCode: "ZA",
+    id: 650673,
+  },
 ];
 
 const northAmerica = [{}];
@@ -147,110 +147,107 @@ const asia = [
     dish: "Fried Rice", //Chinese comfort food""
     language: "Mandarin",
     flagCode: "CN",
-    id:"643786" ,
+    id: 643786,
   },
   {
     country: "Pakistan",
-    dish: "Biryani ",//"Roasted Acorn Squash Stuffed with spicy Biryani (Veg/vegan)""
+    dish: "Biryani ", //"Roasted Acorn Squash Stuffed with spicy Biryani (Veg/vegan)""
     language: "urdu",
     flagCode: "PK",
-    id:"658482" ,
+    id: 658482,
   },
   {
     country: "Indonesia",
     dish: "sambal", //Coconut Rice and Carrot Sambal
     language: "Indonesian",
     flagCode: "ID",
-    id:"639807" ,
+    id: 639807,
   },
   {
     country: "malaysia",
     dish: "Malaysian Sambal Kangkong (Water Spinach)",
     language: "malay",
     flagCode: "MY",
-    id:"650684" ,
+    id: 650684,
   },
   {
     country: "India",
-    dish: "Dahl",//"Indian Lentil Dahl"
+    dish: "Dahl", //"Indian Lentil Dahl"
     language: "hindi",
     flagCode: "IN",
-    id:"647830" ,
+    id: 647830,
   },
   {
     country: "Japan",
-    dish: "Sushi",//"Japanese Sushi
+    dish: "Sushi", //"Japanese Sushi
     language: "Japanese",
     flagCode: "JP",
-    id:"648506" ,
+    id: 648506,
   },
   {
     country: "Afghanistan",
     dish: "Pilaf", //"Rice Pilaf"
     language: "Pashto ",
     flagCode: "AF",
-    id:"658277" ,
+    id: 658277,
   },
   {
     country: "Turkey",
     dish: "baklava", //Honeyed Bacon" Baklava"
     language: "Turkish",
     flagCode: "TR",
-    id:"631783" ,
+    id: 631783,
   },
   {
     country: "Thailand",
     dish: "Thai Massaman Curry",
     language: "thai",
     flagCode: "TH",
-    id:"663121" ,
+    id: 663121,
   },
-    {
-        country: "cyprus",
-        dish: "Marinated Souvlaki",
-        language: "greek",
-        flagCode: "CY",
-        id:"651076" ,
-      },
+  {
+    country: "cyprus",
+    dish: "Marinated Souvlaki",
+    language: "greek",
+    flagCode: "CY",
+    id: 651076,
+  },
 ];
 
 const oceania = [
-    {
-        country: "australia",
-        dish: "lamb", //roast lamb
-        language: "english",
-        flagCode: "AU",
-        id: 641973,
-      },
-      {
-        country: "new zealand",
-        dish: "Pie-meat",
-        language: "english",
-        flagCode: "NZ",
-        id: 652968,
-      },{
-        country: "Solomon Islands",
-        dish: "Coconut Cassava Pancakes",
-        language: "english", //Pijin
-        flagCode: "SB",
-        id: 1096262,
-      },
-      {
-        country: "fiji",
-        dish: "Grilled fish", //Grilled Fish Tacos with Spicy Tequila-Lime Guacamole"
-        language: "Fijian",
-        flagCode: "FJ",
-        id:"645711" ,
-      },
-      {
-        country: "Vanuatu",
-        dish: "Snapper", //"Salt-Crusted Snapper With Blood Orange and Bay"
-        language: "Vanuatu",
-        flagCode: "VU",
-        id:"659192" ,
-      },
-   
-     
-
+  {
+    country: "australia",
+    dish: "lamb", //roast lamb
+    language: "english",
+    flagCode: "AU",
+    id: 641973,
+  },
+  {
+    country: "new zealand",
+    dish: "Pie-meat",
+    language: "english",
+    flagCode: "NZ",
+    id: 652968,
+  },
+  {
+    country: "Solomon Islands",
+    dish: "Coconut Cassava Pancakes",
+    language: "english", //Pijin
+    flagCode: "SB",
+    id: 1096262,
+  },
+  {
+    country: "fiji",
+    dish: "Grilled fish", //Grilled Fish Tacos with Spicy Tequila-Lime Guacamole"
+    language: "Fijian",
+    flagCode: "FJ",
+    id: 645711,
+  },
+  {
+    country: "Vanuatu",
+    dish: "Snapper", //"Salt-Crusted Snapper With Blood Orange and Bay"
+    language: "Vanuatu",
+    flagCode: "VU",
+    id: 659192,
+  },
 ];
-
--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -1,25 +1,15 @@
 $(function () {
   const apikey = "0ca619dbe1c54687905affcae7c39231";
   // "813168050135472e9820b823b47943fa";
-  var id = 633661;
-  var queryurl =
-    `https://api.spoonacular.com/recipes/${id}/information?apiKey=` + apikey;
-
-  $.ajax({
-    url: queryurl,
-    method: "GET",
-    success: function (response) {
-      var flagImage = "https://www.countryflags.io/be/flat/64.png"; // image
-      console.log(response);
-    },
-  });
 
   //display continents 1-7
   //user must select 1 min - max 7
   //user clicks 'submit' event listner
   // build up country selection form continents
   function countrySelection() {
+    //empty array to dynamically pick a country from the users selection
     var selectedCountriesArr = [];
+
     if ($("#africa").is(":checked")) {
       selectedCountriesArr.push(...africa);
     }
@@ -36,16 +26,54 @@ $(function () {
       selectedCountriesArr.push(...northAmerica);
     }
     if ($("#south-america").is(":checked")) {
-        selectedCountriesArr.push(...southAmerica);
-      }
+      selectedCountriesArr.push(...southAmerica);
+    }
     //   if ($("#antarctica").is(":checked")) {
     //     selectedCountriesArr.push(...antarctica);
     //   }
-    console.log(selectedCountriesArr);
+    //generates a random number from 0 to the length of the dynamic array
+    var randomNumber = Math.floor(Math.random() * selectedCountriesArr.length);
+    //selects the country object;
+    var randomCountry = selectedCountriesArr[randomNumber];
+    //get the id from the country
+    var id = randomCountry.id;
+    //get the flagcode from the country object
+    var flagCode = randomCountry.flagCode;
 
+    var queryurl =
+      `https://api.spoonacular.com/recipes/${id}/information?apiKey=` + apikey;
+    
+
+    $.ajax({
+      url: queryurl,
+      method: "GET",
+      success: function (response) {
+
+        console.log(response);
+
+        var dishTitle = response.title;
+        console.log(dishTitle)
+
+        var dishImgSrc = response.image;
+        console.log(dishImgSrc)
+
+        var recipeArr = response.analyzedInstructions[0].steps;
+        console.log(recipeArr);
+
+
+
+
+        //creates the img source
+        var flagImageSrc = `https://www.countryflags.io/${flagCode}/flat/64.png`; // image
+        //creates an img element with the flag source
+        var flagImgElement = $("<img>")
+        flagImgElement.attr("src", flagImageSrc);
+        $("body").append(flagImgElement)
+      },
+    });
   }
-  
-  
+  $("#submitBtn").on("click", countrySelection);
+
   //random country picked
   //make API calls
   //build object dynamically on page
